93753ab2752d58b658f2a3d9bf6f3b277fee24f7
dkTkrkdhfl <482gate@gmail.com>
2024-10-31 21:39:03 +0900

    feat: 플러터 좌표 형변환 및 저장 구현

--- src/global/BtParser.java
+++ src/global/BtParser.java
@@ -12,7 +12,7 @@ public class BtParser {
      */
     public static MsgType getMsgType(String msg){
         // 헤더인 경우
-        if(msg.split("\r")[0].equals("HEADER")){
+        if(msg.split(":")[0].equals("HEADER")){
             return MsgType.HEADER;
         }
 
@@ -32,10 +32,11 @@ public class BtParser {
     }
 
     public static int getX(String msg){
+    	System.out.println("message is " + msg);
         if( !getMsgType(msg).equals(MsgType.POINT)){ //점 데이터가 아니면 예외처리 해야 함.
             return -1;
         }
-        return Integer.parseInt(msg.split("\r")[0].split(" ")[0]);
+        return Math.round(Float.parseFloat(msg.split(" ")[0])); // 이거 계산시간 길면 걍 반올림 말고 점 떼는것도 생각해야함
 
     }
 
@@ -44,7 +45,7 @@ public class BtParser {
         if( !getMsgType(msg).equals(MsgType.POINT)){ //점 데이터가 아니면 예외처리 해야 함.
             return -1;
         }
-        return Integer.parseInt(msg.split("\r")[0].split(" ")[1]);
+        return Math.round(Float.parseFloat(msg.split(" ")[1]));
 
     }
 }
